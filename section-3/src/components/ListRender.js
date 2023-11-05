333510355f0765a51ed249458c1f0761490c84ac
Raffa-Raffa <rafaelacnh@gmail.com>
2023-11-05 19:50:30 -0300

    previous state - deletando randomicamente itens da lista

--- section-3/src/components/ListRender.js
+++ section-3/src/components/ListRender.js
@@ -3,12 +3,21 @@ import React, { useState } from 'react'
 const ListRender = () => {
     const [list] = useState(["Bradock", "Brianna", "Darth", "Shakira"])
 
-    const [dogs] = useState([
+    const [dogs, setDogs] = useState([
         { id: 1, name: "Bradock", age: 6, color: "caramelo", dogbreed: "SRD" },
         { id: 2, name: "Brianna", age: 3, color: "caramelo", dogbreed: "SRD" },
         { id: 3, name: "Darth", age: 3, color: "Preto", dogbreed: "SRD" },
         { id: 4, name: "Shakira", age: 2, color: "caramelo", dogbreed: "SRD" },
     ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setDogs((prevDogs)=> {
+            console.log(prevDogs)
+        return prevDogs.filter((user)=> randomNumber !== user.id)})
+    }
+
     return (
         <div className='listrender'>
             <div>
@@ -23,6 +32,7 @@ const ListRender = () => {
                         <li key={dog.id}>Name: {dog.name} - Age: {dog.age} - Color: {dog.color} - DogBreed: {dog.dogbreed}</li>
                     ))}
                 </ul>
+                <button onClick={deleteRandom}>Delete random Dogs</button>
             </div>
         </div>
     )
