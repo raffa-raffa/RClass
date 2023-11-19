debbad5764faf358d87f6d9c4b17f508f597e7c4
Raffa-Raffa <rafaelacnh@gmail.com>
2023-11-19 01:31:21 -0300

    chore: :hammer: criando função que define vitoria e logica para exibir mensagem de vitoria

--- secret-word/src/components/SecretWord.js
+++ secret-word/src/components/SecretWord.js
@@ -9,36 +9,36 @@ const SecretWord = () => {
     const [score, setScore] = useState(0);
 
 
-    const wordlist = [{ word: " Elefante ", dica: ": Maior mamífero terrestre" },
-    { word: " Astronauta ", dica: ": Pessoa que viaja para o espaço" },
-    { word: " Guitarra ", dica: ": Instrumento musical de cordas" },
-    { word: " Abacaxi ", dica: ": Fruta tropical amarela e espinhosa" },
-    { word: " Esquilo ", dica: ": Roedor fofo com cauda peluda" },
-    { word: " Arco-íris ", dica: ": Fenômeno óptico com várias cores" },
-    { word: " Camaleão ", dica: ": Réptil que muda de cor" },
-    { word: " Violino ", dica: ": Instrumento de cordas tocado com um arco" },
-    { word: " Girassol ", dica: ": Flor amarela que segue o sol" },
-    { word: " Cachoeira ", dica: ": Fluxo de água caindo de uma altura" },
-    { word: " Pinguim ", dica: ": Ave marinha não voadora" },
-    { word: " Chocolate ", dica: ": Doce feito de cacau e açúcar" },
-    { word: " Onda ", dica: ": Movimento de uma superfície da água" },
-    { word: " Futebol ", dica: ": Esporte com uma bola e dois times" },
-    { word: " Cenoura ", dica: ": Vegetal alaranjado e crocante" },
-    { word: " Computador ", dica: ": Dispositivo eletrônico para processamento de dados" },
-    { word: " Piano ", dica: ": Instrumento musical de teclas" },
-    { word: " Floresta ", dica: ": Área extensa coberta por árvores" },
-    { word: " Foguete ", dica: ": Veículo espacial propelido por motores" },
-    { word: " Coruja ", dica: ": Ave noturna de rapina" },
-    { word: " Melancia ", dica: ": Fruta grande com polpa vermelha" },
-    { word: " Inverno ", dica: ": Estação do ano mais fria" },
-    { word: " Bailarina ", dica: ": Pessoa que dança ballet" },
-    { word: " Tesouro ", dica: ": Objeto de valor escondido" },
-    { word: " Sorrir ", dica: ": Expressão facial feliz" },
-    { word: " Travesseiro ", dica: ": Usado para apoiar a cabeça durante o sono" },
-    { word: " Helicóptero ", dica: ": Aeronave com hélices giratórias" },
-    { word: " Biblioteca ", dica: ": Local onde se encontram livros" },
-    { word: " Abraço ", dica: ": Demonstração de carinho com os braços" },
-    { word: " Montanha ", dica: ": Elevação natural e proeminente da Terra" },]
+    const wordlist = [{ word: "Elefante", dica: ": Maior mamífero terrestre" },
+    { word: "Astronauta", dica: ": Pessoa que viaja para o espaço" },
+    { word: "Guitarra", dica: ": Instrumento musical de cordas" },
+    { word: "Abacaxi", dica: ": Fruta tropical amarela e espinhosa" },
+    { word: "Esquilo", dica: ": Roedor fofo com cauda peluda" },
+    { word: "Arco-íris", dica: ": Fenômeno óptico com várias cores" },
+    { word: "Camaleão", dica: ": Réptil que muda de cor" },
+    { word: "Violino", dica: ": Instrumento de cordas tocado com um arco" },
+    { word: "Girassol", dica: ": Flor amarela que segue o sol" },
+    { word: "Cachoeira", dica: ": Fluxo de água caindo de uma altura" },
+    { word: "Pinguim", dica: ": Ave marinha não voadora" },
+    { word: "Chocolate", dica: ": Doce feito de cacau e açúcar" },
+    { word: "Onda", dica: ": Movimento de uma superfície da água" },
+    { word: "Futebol", dica: ": Esporte com uma bola e dois times" },
+    { word: "Cenoura", dica: ": Vegetal alaranjado e crocante" },
+    { word: "Computador", dica: ": Dispositivo eletrônico para processamento de dados" },
+    { word: "Piano", dica: ": Instrumento musical de teclas" },
+    { word: "Floresta", dica: ": Área extensa coberta por árvores" },
+    { word: "Foguete", dica: ": Veículo espacial propelido por motores" },
+    { word: "Coruja", dica: ": Ave noturna de rapina" },
+    { word: "Melancia", dica: ": Fruta grande com polpa vermelha" },
+    { word: "Inverno", dica: ": Estação do ano mais fria" },
+    { word: "Bailarina", dica: ": Pessoa que dança ballet" },
+    { word: "Tesouro", dica: ": Objeto de valor escondido" },
+    { word: "Sorrir", dica: ": Expressão facial feliz" },
+    { word: "Travesseiro", dica: ": Usado para apoiar a cabeça durante o sono" },
+    { word: "Helicóptero", dica: ": Aeronave com hélices giratórias" },
+    { word: "Biblioteca", dica: ": Local onde se encontram livros" },
+    { word: "Abraço", dica: ": Demonstração de carinho com os braços" },
+    { word: "Montanha", dica: ": Elevação natural e proeminente da Terra" },]
 
     const getRandomWord = () => {
         const randomIndex = Math.floor(Math.random() * wordlist.length);
@@ -48,31 +48,28 @@ const SecretWord = () => {
     const [currentWord, setCurrentWord] = useState(getRandomWord());
 
     const checkWord = () => {
-        let updatedDisplayedWord = currentWord.word
-        .toLowerCase()
-        .split('')
-        .map((l) => (guessedLetters.includes(l) ? l : "_"))
-        .join('');
+        const sanitizedWord = currentWord.word.toLowerCase().replace(/ /g, '');
+        const updatedDisplayedWord = sanitizedWord
+            .split('')
+            .map((l) => (guessedLetters.includes(l) || l === ' ' ? l : "_"))
+            .join('');
         setDisplayedWord(updatedDisplayedWord);
-    };
-
-    const gameattepnts = () => {
-        if (remainingAttempts === 3) {
-            setScore(score + 25);
-        }
+        return sanitizedWord;
     };
 
     const verifyLetter = () => {
         const lowercaseLetter = letter.toLowerCase();
-        if (currentWord.word.toLowerCase().includes(lowercaseLetter)) {
-            gameattepnts();
+        const isCorrect = currentWord.word.toLowerCase().includes(lowercaseLetter);
+
+        setGuessedLetters([...guessedLetters, lowercaseLetter]);
+        setLetter("");
+
+        if (isCorrect) {
+            setScore(score + 25);
         } else {
             setRemainingAttempts(remainingAttempts - 1);
         }
-        setGuessedLetters([...guessedLetters, lowercaseLetter]);
-        setLetter("");
     };
-
 
     const onchangeData = (event) => {
         const maxLength = 1;
@@ -80,8 +77,14 @@ const SecretWord = () => {
         setLetter(value);
     };
 
+    const checkVictory = () => {
+        const sanitizedWord = currentWord.word.toLowerCase().replace(/ /g, '');
+        return Array.from(sanitizedWord).every(letter => guessedLetters.includes(letter));
+    };
+
     useEffect(() => {
-    checkWord();
+        checkWord();
+        checkVictory();
     }, [currentWord, guessedLetters]);
 
     const handleNewWord = () => {
@@ -100,14 +103,26 @@ const SecretWord = () => {
                 <p>Você ainda tem {remainingAttempts} tentativas</p>
             </div>
             <div className="container">
-                {remainingAttempts > 0  &&  score >= 0 ? (
-                <div className="secretword">
-                    {displayedWord.split('').map((letter, index) => (
-                        <div key={index} className="letter">
-                            {letter === '_' ? ' ' : letter}
+                {console.log("Guessed Letters:", guessedLetters)}
+                <div className="container">
+                    {remainingAttempts > 0 && !checkVictory() ? (
+                        <div className="secretword">
+                            {displayedWord.split('').map((letter, index) => (
+                                <div key={index} className="letter">
+                                    {letter === '_' ? ' ' : letter}
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>): <h1>Ahh, Não foi dessa vez!  Tente de novo!!!</h1> }
+                    ) : (
+                        <div>
+                            {checkVictory() ? (
+                                <h1>Parabéns! Você acertou a Palavra!!!</h1>
+                            ) : (
+                                <h1>Ahh, Não foi dessa vez! Tente de novo!!!</h1>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
             <div className="footer">
                 <p>Tente advinhar uma letra da palavra</p>
