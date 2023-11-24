81368a1ebb5d5409da3a97bcd31610a563d4d9b0
Raffa-Raffa <rafaelacnh@gmail.com>
2023-11-24 00:16:56 -0300

    chore: carregamento dinâmico e custom hooks resgatando dados

--- httpreact/src/App.js
+++ httpreact/src/App.js
@@ -1,52 +1,60 @@
-import React from 'react';
-import './App.css';
-import {useState, useEffect} from "react"
+  import React, { useState } from 'react';
+  import './App.css';
+  import { useFetch } from './hooks/useFetch';
 
+  const url = "http://localhost:3000/products";
 
-const url = "http://localhost:3000/products"
-function App() {
-const [products, setProducts] = useState([])
-const [price, setPrice] = useState([])
-const [name, setName] = useState([])
-useEffect(()=>{
-  async function fetchData(){
-  const res = await fetch(url)
-  const data = await res.json()
-  setProducts(data)
-  }
-  fetchData()
-}, [])
+  function App() {
+    const [products, setProducts] = useState([]);
+    const [price, setPrice] = useState("");
+    const [name, setName] = useState("");
 
-const handleSubmit = async(e) =>{
-  e.preventDefault();
+    const { data: items } = useFetch(url);
+  console.log(products)
+    console.log(items);
 
-  const product = {
-  name, 
-  price
-}
-const res = await fetch(url, {
-method:"POST",
-headers: {
-  "Content-Type": "application/json"
-}, 
-body: JSON.stringify(product)}
+    const handleSubmit = async (e) => {
+      e.preventDefault();
 
-)
-console.log(res)
-}
+      const product = {
+        name,
+        price
+      };
 
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      });
+
+      console.log(res);
+
+      const addedProduct = await res.json();
+      setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+      setName("");
+      setPrice("");
+    };
 
     return (
       <div className="App">
         <h1>Lista de Produtos</h1>
-        <ul>{products.map((product)=> (
-        <li>{product.name} - R$: {product.price}</li>))}</ul>
+        <ul>
+          {items && items.map((product) => (
+            <li key={product.id}>{product.name} - R$: {product.price}</li>
+          ))}
+        </ul>
         <div className="add-product">
           <form onSubmit={handleSubmit}>
-            <label>Nome:
-              <input type="text" value={name} name="name" onChange={(e)=> setName(e.target.value)} />
-            </label>  <label>Preço:
-              <input type="number" value={price} name="price" onChange={(e)=> setPrice(e.target.value)} />
+            <label>
+              Nome:
+              <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+              Preço:
+              <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
             </label>
             <input type="submit" value="Criar" />
           </form>
@@ -55,5 +63,4 @@ console.log(res)
     );
   }
 
-
-export default App;
+  export default App;
