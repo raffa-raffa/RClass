c63810a16a2d587c91143d7a3b29496d92b362e3
Raffa-Raffa <rafaelacnh@gmail.com>
2023-12-10 23:46:51 -0300

    chore: redirecionamento de url

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -1,6 +1,6 @@
     import React from 'react';
     import './App.css';
-    import { BrowserRouter, Routes, Route} from 'react-router-dom'
+    import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
     import Home from './pages/Home';
     import About from './pages/About';
     import Navbar from './components/Navbar';
@@ -20,10 +20,11 @@
               <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
-                <Route path="/products/:id" element={<Product/>}/>
                 <Route path="/products/:id/info" element={<Info/>}/>
-                <Route path="*" element={<NotFound/>}/>
+                <Route path="/products/:id" element={<Product/>}/>
                 <Route path="/search" element={<Search/>}/>
+                <Route path="/company" element={<Navigate to="/about"/>}></Route>
+                <Route path="*" element={<NotFound/>}/>
               </Routes>
               </BrowserRouter>
             </div>
