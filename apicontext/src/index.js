5cdeb753d7a23014b2ea5d271bbbebd8a36fbaeb
Raffa-Raffa <rafaelacnh@gmail.com>
2023-12-12 00:28:18 -0300

    chore: :hammer: criando contexto e Provider

--- apicontext/src/index.js
+++ apicontext/src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import App from './App';
+  import './index.css';
+  import { CounterContextProvider } from './context/CounterContext';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+  <React.StrictMode>
+
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
+  </React.StrictMode>,
+    document.getElementById('root')
+  );
--- apicontext/src/pages/Home.js
+++ apicontext/src/pages/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+  import React, { useContext } from 'react';
+  import CounterContext from '../context/CounterContext'; 
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const Home = () => {
+    const { counter, setCounter } = useContext(CounterContext);
 
-export default Home
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>Counter value: {counter}</p>
+      </div>
+    );
+  };
+
+  export default Home;
