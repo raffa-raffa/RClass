c100b965d07eeff34fcd8e9e68bb2c50673e0e8f
Raffa-Raffa <rafaelacnh@gmail.com>
2023-12-18 23:23:28 -0300

    chore: :sparkles: criando projeto miniblog, conectando firebase e config router-dom

---
+++ miniblog/src/App.js
@@ -0,0 +1,24 @@
+  import React, { Component } from 'react';
+  import './App.css';
+  import{BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+  import Home from "./pages/Home/Home"
+  import About from "./pages/About/About"
+
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <BrowserRouter>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+            </Routes>
+          </div>
+          </BrowserRouter>
+          </div>
+      );
+    }
+  }
+
+  export default App;
---
+++ miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKr2K5xn1QNdQwxigeBPWO2LYD2MMtlBQ",
+  authDomain: "miniblog-64847.firebaseapp.com",
+  projectId: "miniblog-64847",
+  storageBucket: "miniblog-64847.appspot.com",
+  messagingSenderId: "986554324295",
+  appId: "1:986554324295:web:804d906a91ae9847b9a862"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export {db}
