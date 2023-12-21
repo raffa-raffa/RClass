ee5b220b1c9f1d9c52a241fa122c814a27f0da2a
Raffa-Raffa <rafaelacnh@gmail.com>
2023-12-20 23:59:38 -0300

    chore: add navbar e footer

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -3,12 +3,14 @@
   import{BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
   import Home from "./pages/Home/Home"
   import About from "./pages/About/About"
-
+  import Navbar from './components/Navbar';
+  import Footer from './components/Footer';
   class App extends Component {
     render() {
       return (
         <div className="App">
           <BrowserRouter>
+        <Navbar/>
           <div className="container">
             <Routes>
               <Route path="/" element={<Home/>}/>
@@ -16,6 +18,7 @@
             </Routes>
           </div>
           </BrowserRouter>
+          <Footer/>
           </div>
       );
     }
---
+++ miniblog/src/components/Footer.js
@@ -0,0 +1,13 @@
+    import React from 'react'
+    import styles from "./Footer.module.css"
+
+    const Footer = () => {
+    return (
+        <footer>
+            <h3>Escreva sobre o que você tem interesse!</h3>
+            <p>Mini Blog &copy</p>
+        </footer>
+    )
+    }
+
+    export default Footer
