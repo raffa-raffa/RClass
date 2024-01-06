9d24764222734a81c63075266a997f7bfa396ff4
Raffa-Raffa <rafaelacnh@gmail.com>
2024-01-05 23:46:05 -0300

    chore: bloquei de paginas que demanda authenticação

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useAuthentication } from './hooks/useAuthentication';
@@ -15,7 +15,7 @@ import Dashboard from './pages/Dashboard/Dashboard';
 const AppContent = () => {
   const [user, setUser] = useState(undefined);
   const { auth } = useAuthentication();
-  
+
   const loadingUser = user === undefined;
 
   useEffect(() => {
@@ -37,12 +37,12 @@ const AppContent = () => {
           <Navbar />
           <div className="container">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Home />}/>
               <Route path="/about" element={<About />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastrar" element={<Register />} />
-              <Route path="/posts/create" element={<CreatePost />} />
-              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/login" element={!user? <Login />: <Navigate to="/"/>}/>
+              <Route path="/register" element={!user?<Register />: <Navigate to="/"/>}/>
+              <Route path="/posts/create" element={user? <CreatePost />: <Navigate to="/login"/>} />
+              <Route path="/dashboard" element={user? <Dashboard />: <Navigate to="/login"/>} />
             </Routes>
           </div>
         </BrowserRouter>
